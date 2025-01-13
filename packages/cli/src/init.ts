eb6b7dd0dcc360cde8f9307addd7770339254388
zhang <xbzhang2020@icloud.com>
2025-01-13 20:18:40 +0800

    chore(cli): use dynamic import for rsbuild

--- packages/cli/src/init.ts
+++ packages/cli/src/init.ts
@@ -1,6 +1,6 @@
 import { existsSync } from 'node:fs';
 import { copyFile, cp, mkdir, readFile, readdir, writeFile } from 'node:fs/promises';
-import { dirname, relative, resolve, basename } from 'node:path';
+import { basename, dirname, relative, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { checkbox, input, select } from '@inquirer/prompts';
 import chalk from 'chalk';
