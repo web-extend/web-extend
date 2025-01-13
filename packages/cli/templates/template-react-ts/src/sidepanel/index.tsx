caa8d1984f883b11927be9fbf557ede1490bfa08
zhang <xbzhang2020@icloud.com>
2025-01-13 21:24:01 +0800

    feat(cli): support svelte template

--- packages/cli/templates/template-react-ts/src/sidepanel/index.tsx
+++ packages/cli/templates/template-react-ts/src/sidepanel/index.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const rootEl = document.getElementById("root");
+const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = createRoot(rootEl);
   root.render(
     <StrictMode>
       <App />
-    </StrictMode>
+    </StrictMode>,
   );
 }
