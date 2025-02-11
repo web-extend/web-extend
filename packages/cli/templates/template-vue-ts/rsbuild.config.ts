d337e6fa073a605704559e9f49ee53d3c8efd052
zhang <xbzhang2020@icloud.com>
2025-02-11 22:00:59 +0800

    fix(cli): support vue hmr

--- packages/cli/templates/template-vue-ts/rsbuild.config.ts
+++ packages/cli/templates/template-vue-ts/rsbuild.config.ts
@@ -4,4 +4,10 @@ import { pluginWebExtend } from '@web-extend/rsbuild-plugin';
 
 export default defineConfig({
   plugins: [pluginVue(), pluginWebExtend()],
+  output: {
+    // https://github.com/web-infra-dev/rsbuild/issues/3217
+    sourceMap: {
+      js: false
+    }
+  }
 });
