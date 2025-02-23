810bcd3bfa5412259d16d92125904835da336fd7
zhang <xbzhang2020@icloud.com>
2025-02-23 17:48:16 +0800

    dos: add vitesse-webext migration in English

--- packages/cli/src/generate.ts
+++ packages/cli/src/generate.ts
@@ -4,7 +4,6 @@ import { checkbox } from '@inquirer/prompts';
 import { resolveSrcDir } from '@web-extend/manifest';
 import sharp from 'sharp';
 import { copyEntryFiles, entrypoints, getTemplatePath, resolveEntryTemplate } from './init.js';
-import { resolveSrcDir } from '@web-extend/manifest';
 
 export interface GenerateOptions {
   entries: string[];
