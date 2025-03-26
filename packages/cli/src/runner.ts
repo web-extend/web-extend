bc92aa939347583f9c8d61ed559d9d449e824db5
zhangxingbin <xbzhang2020@icloud.com>
2025-03-26 09:09:01 +0800

    fix: error ERR_UNSUPPORTED_ESM_URL_SCHEME

--- packages/cli/src/runner.ts
+++ packages/cli/src/runner.ts
@@ -1,10 +1,10 @@
 import { existsSync } from 'node:fs';
 import { relative, resolve } from 'node:path';
+import { pathToFileURL } from 'node:url';
 import { defaultExtensionTarget } from '@web-extend/manifest/common';
 import type { ExtensionTarget } from '@web-extend/manifest/types';
 import chalk from 'chalk';
 import { resolveBuildInfo } from './result.js';
-import { pathToFileURL } from 'node:url';
 
 type TargetType = 'firefox-desktop' | 'firefox-android' | 'chromium';
 
