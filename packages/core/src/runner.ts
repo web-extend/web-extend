3f585e900b1c5ce3349789f3acdeec34d50c4a66
zhangxingbin <xbzhang2020@icloud.com>
2025-06-04 08:57:59 +0800

    chore: test

--- packages/core/src/runner.ts
+++ packages/core/src/runner.ts
@@ -2,9 +2,9 @@ import { existsSync } from 'node:fs';
 import { relative, resolve } from 'node:path';
 import { pathToFileURL } from 'node:url';
 import { defaultExtensionTarget } from '@web-extend/manifest/common';
+import chalk from 'chalk';
 import type { ExtensionTarget } from '@web-extend/manifest/types';
 import { resolveBuildInfo } from './result.js';
-import chalk from 'chalk';
 
 type TargetType = 'firefox-desktop' | 'firefox-android' | 'chromium';
 
