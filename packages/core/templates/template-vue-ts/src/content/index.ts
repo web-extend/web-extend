4fd568e8e7af3af3b0bf41fde1687fc34129975e
zhangxingbin <xbzhang2020@icloud.com>
2025-06-14 14:21:21 +0800

    refactor: rootEl -> root

--- packages/core/templates/template-vue-ts/src/content/index.ts
+++ packages/core/templates/template-vue-ts/src/content/index.ts
@@ -2,12 +2,12 @@ import type { ContentScriptConfig } from 'web-extend';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-let rootEl = document.getElementById('web-extend-content');
-if (!rootEl) {
-  rootEl = document.createElement('div');
-  rootEl.id = 'web-extend-content';
-  document.body.appendChild(rootEl);
-  createApp(App).mount(rootEl);
+let root = document.getElementById('web-extend-content');
+if (!root) {
+  root = document.createElement('div');
+  root.id = 'web-extend-content';
+  document.body.appendChild(root);
+  createApp(App).mount(root);
 }
 
 export const config: ContentScriptConfig = {
