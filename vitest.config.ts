e2c93e72765b426767fc6853daee3f5ce3501ac6
zhangxingbin <xbzhang2020@icloud.com>
2025-07-06 22:33:38 +0800

    chore: add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, coverageConfigDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     workspace: ['packages/*'],
+    coverage: {
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        '**/dist/**',
+        '**/static/**',
+        '**/rslib.config.ts',
+        'packages/core/templates/**',
+      ],
+    },
   },
 });
